c2bb76f624127a954327665729131964b2b9a0f8
iks-rain <ssp474138610@163.com>
2025-12-13 21:58:09 +0800

    test: 更多测试

--- BInaryKit-Test/UnsafeToolkit/Pin-Test.cs
+++ BInaryKit-Test/UnsafeToolkit/Pin-Test.cs
@@ -1,12 +1,15 @@
-﻿using Iks.BinaryToolkit.UnsafeToolkit;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Iks.BinaryToolkit.UnsafeToolkit;
 using Xunit.Abstractions;
 
 namespace BinaryKit_Test.UnsafeToolkit;
 
 public class PinTest(ITestOutputHelper output)
 {
+    // pin must be not null
     [Fact]
-    public unsafe void NotNullPin()
+    public void NotNullPin()
     {
         try
         {
@@ -17,7 +20,51 @@ public class PinTest(ITestOutputHelper output)
         // throws is right,null cannot be pinned
         catch (Exception e)
         {
-            Assert.True(e is ArgumentNullException);
+            return;
         }
+        Assert.Fail();
     }
+    
+    [Fact]
+    public void NotUseAfterRelease()
+    {
+        var pin = new object().AsPinned();
+        pin.Dispose();
+        // use after release
+        try
+        {
+            var pinTarget = pin.Target;
+        }
+        catch 
+        {
+            return;
+        }
+        Assert.Fail();
+    }
+    [Fact]
+    public void NotMultipleRelease()
+    {
+        var pin = new object().AsPinned();
+        pin.Dispose();
+        // release again
+        try
+        {
+            pin.Dispose();
+        }
+        catch 
+        {
+            return;
+        }
+        Assert.Fail();
+    }
+    
+    [Fact]
+    public void CopyRelease()
+    {
+        var pin_old = new object().AsPinned();
+        var pin_new = pin_old;
+        pin_new.Dispose();
+    }
+
+    
 }
