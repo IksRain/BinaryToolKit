859cfb50a2e5084d412d8fdac24ef8bf492c6e61
iks-rain <ssp474138610@163.com>
2025-12-13 21:23:18 +0800

    test: 增加对Pin的非Null性Test

---
+++ BInaryKit-Test/UnsafeToolkit/Pin-Test.cs
@@ -0,0 +1,23 @@
+﻿using Iks.BinaryToolkit.UnsafeToolkit;
+using Xunit.Abstractions;
+
+namespace BinaryKit_Test.UnsafeToolkit;
+
+public class PinTest(ITestOutputHelper output)
+{
+    [Fact]
+    public unsafe void NotNullPin()
+    {
+        try
+        {
+            // set a null ref
+            string str = null!;
+            using var pin = str.AsPinned();
+        }
+        // throws is right,null cannot be pinned
+        catch (Exception e)
+        {
+            Assert.True(e is ArgumentNullException);
+        }
+    }
+}
